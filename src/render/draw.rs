5e70e889fa7167fc7f3ccbdee194f7d33c410635
KirmesBude <kirmesbude@gmail.com>
2024-08-04 15:28:44 +0200

    Implement prepare and render commands

--- src/render/draw.rs
+++ src/render/draw.rs
@@ -1,9 +1,13 @@
 use bevy::{
     core_pipeline::core_3d::Transparent3d,
-    render::render_phase::{
-        RenderCommand, RenderCommandResult, SetItemPipeline, TrackedRenderPass,
+    ecs::{query::ROQueryItem, system::lifetimeless::Read},
+    render::{
+        render_phase::{RenderCommand, RenderCommandResult, SetItemPipeline, TrackedRenderPass},
+        view::ViewUniformOffset,
     },
 };
+
+use super::prepare::{BillboardMeta, BillboardTextureBindGroup, BillboardViewBindGroup};
 
 pub type DrawBillboardRenderCommand = (
     SetItemPipeline,
@@ -17,18 +21,18 @@ pub type DrawBillboardRenderCommand = (
 pub struct SetBillboardViewGroup<const I: usize>;
 impl<const I: usize> RenderCommand<Transparent3d> for SetBillboardViewGroup<I> {
     type Param = ();
-    type ViewQuery = ();
+    type ViewQuery = (Read<ViewUniformOffset>, Read<BillboardViewBindGroup>);
     type ItemQuery = ();
 
     #[inline]
     fn render<'w>(
         _item: &Transparent3d,
-        _view: (),
+        (view_uniform, billboard_view_bind_group): ROQueryItem<'w, Self::ViewQuery>,
         _entity: Option<()>,
         _param: (),
-        _pass: &mut TrackedRenderPass<'w>,
+        pass: &mut TrackedRenderPass<'w>,
     ) -> RenderCommandResult {
-        /* TODO: Set bindgroup on pass */
+        pass.set_bind_group(I, &billboard_view_bind_group.value, &[view_uniform.offset]);
 
         RenderCommandResult::Success
     }
@@ -38,17 +42,20 @@ pub struct SetBillboardTextureBindGroup<const I: usize>;
 impl<const I: usize> RenderCommand<Transparent3d> for SetBillboardTextureBindGroup<I> {
     type Param = ();
     type ViewQuery = ();
-    type ItemQuery = ();
+    type ItemQuery = Read<BillboardTextureBindGroup>;
 
     #[inline]
     fn render<'w>(
         _item: &Transparent3d,
         _view: (),
-        _entity: Option<()>,
+        texture_bind_group: Option<ROQueryItem<'w, Self::ItemQuery>>,
         _param: (),
-        _pass: &mut TrackedRenderPass<'w>,
+        pass: &mut TrackedRenderPass<'w>,
     ) -> RenderCommandResult {
-        /* TODO: Set bindgroup on pass */
+        let Some(texture_bind_group) = texture_bind_group else {
+            return RenderCommandResult::Failure;
+        };
+        pass.set_bind_group(I, &texture_bind_group.value, &[]);
 
         RenderCommandResult::Success
     }
@@ -58,17 +65,22 @@ pub struct DrawBillboard;
 impl RenderCommand<Transparent3d> for DrawBillboard {
     type Param = ();
     type ViewQuery = ();
-    type ItemQuery = ();
+    type ItemQuery = Read<BillboardMeta>;
 
     #[inline]
     fn render<'w>(
-        _item: &Transparent3d,
+        item: &Transparent3d,
         _view: (),
-        _entity: Option<()>,
+        billboard_meta: Option<ROQueryItem<'w, Self::ItemQuery>>,
         _param: (),
-        _pass: &mut TrackedRenderPass<'w>,
+        pass: &mut TrackedRenderPass<'w>,
     ) -> RenderCommandResult {
-        /* TODO: Set index/vertex and draw on pass */
+        let Some(billboard_meta) = billboard_meta else {
+            return RenderCommandResult::Failure;
+        };
+
+        pass.set_vertex_buffer(0, billboard_meta.vertices.buffer().unwrap().slice(..));
+        pass.draw(item.batch_range.clone(), 0..1);
 
         RenderCommandResult::Success
     }
--- src/render/prepare.rs
+++ src/render/prepare.rs
@@ -1,5 +1,130 @@
-pub fn prepare_billboard_view_bind_groups() {}
+use bevy::{
+    math::Vec2,
+    prelude::{Commands, Component, Entity, Query, Res, With},
+    render::{
+        render_asset::RenderAssets,
+        render_resource::{BindGroup, BindGroupEntries, BufferUsages, BufferVec, ShaderType},
+        renderer::{RenderDevice, RenderQueue},
+        texture::GpuImage,
+        view::{ExtractedView, ViewUniforms},
+    },
+};
 
-pub fn prepare_billboard_texture_bind_groups() {
-    /* TODO: Here I would create the Quad? */
+use super::{extract::ExtractedBillboard, pipeline::BillboardPipeline};
+
+#[derive(Component)]
+pub struct BillboardViewBindGroup {
+    pub value: BindGroup,
+}
+
+pub fn prepare_billboard_view_bind_groups(
+    mut commands: Commands,
+    render_device: Res<RenderDevice>,
+    billboard_pipeline: Res<BillboardPipeline>,
+    view_uniforms: Res<ViewUniforms>,
+    views: Query<Entity, With<ExtractedView>>,
+) {
+    let Some(view_binding) = view_uniforms.uniforms.binding() else {
+        return;
+    };
+
+    for entity in &views {
+        let view_bind_group = render_device.create_bind_group(
+            "billboard_view_bind_group",
+            &billboard_pipeline.view_layout,
+            &BindGroupEntries::with_indices(((0, view_binding.clone()),)),
+        );
+
+        commands.entity(entity).insert(BillboardViewBindGroup {
+            value: view_bind_group,
+        });
+    }
+}
+
+const QUAD_INDICES: [usize; 6] = [0, 2, 3, 0, 1, 2];
+
+const QUAD_VERTEX_POSITIONS: [Vec2; 4] = [
+    Vec2::new(-0.5, -0.5),
+    Vec2::new(0.5, -0.5),
+    Vec2::new(0.5, 0.5),
+    Vec2::new(-0.5, 0.5),
+];
+
+const QUAD_UVS: [Vec2; 4] = [
+    Vec2::new(0., 1.),
+    Vec2::new(1., 1.),
+    Vec2::new(1., 0.),
+    Vec2::new(0., 0.),
+];
+
+/* TODO: Having this as a component is not efficient I guess */
+#[derive(Component)]
+pub struct BillboardTextureBindGroup {
+    pub value: BindGroup,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, ShaderType)]
+pub struct BillboardVertex {
+    pub position: [f32; 3],
+    pub uv: [f32; 2],
+}
+
+#[derive(Component)]
+pub struct BillboardMeta {
+    pub vertices: BufferVec<BillboardVertex>,
+}
+
+impl Default for BillboardMeta {
+    fn default() -> Self {
+        Self {
+            vertices: BufferVec::new(BufferUsages::VERTEX),
+        }
+    }
+}
+
+pub fn prepare_billboard_texture_bind_groups(
+    mut commands: Commands,
+    render_device: Res<RenderDevice>,
+    render_queue: Res<RenderQueue>,
+    billboard_pipeline: Res<BillboardPipeline>,
+    extracted_billboard_q: Query<(Entity, &ExtractedBillboard)>,
+    gpu_images: Res<RenderAssets<GpuImage>>,
+) {
+    for (entity, extracted_billboard) in &extracted_billboard_q {
+        let Some(gpu_image) = gpu_images.get(extracted_billboard.texture) else {
+            continue;
+        };
+
+        let texture_bind_group = render_device.create_bind_group(
+            "billboard_texture_bind_group",
+            &billboard_pipeline.material_layout,
+            &BindGroupEntries::with_indices((
+                (0, &gpu_image.texture_view),
+                (1, &gpu_image.sampler),
+            )),
+        );
+
+        commands.entity(entity).insert(BillboardTextureBindGroup {
+            value: texture_bind_group,
+        });
+
+        /* Quad vertices*/
+        let uvs = QUAD_UVS;
+        //let quad_size = gpu_image.size;
+        // Apply size and global transform
+        let positions = QUAD_VERTEX_POSITIONS.map(|quad_pos| quad_pos.extend(0.).into());
+
+        let mut billboard_meta = BillboardMeta::default();
+        for i in QUAD_INDICES {
+            billboard_meta.vertices.push(BillboardVertex {
+                position: positions[i],
+                uv: uvs[i].into(),
+            });
+        }
+        billboard_meta
+            .vertices
+            .write_buffer(&render_device, &render_queue);
+        commands.entity(entity).insert(billboard_meta);
+    }
 }
